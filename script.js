417817df3e5ab03cfb333682dff4b266da79c9d4
ssoossTX <sananoskov8992@gmail.com>
2025-02-03 06:30:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -575,7 +575,7 @@ document.addEventListener('DOMContentLoaded', () => {
         achievements: [],
         achievementCount: 0,
         autoClickerInterval: null,
-        diamonds: 0,
+        diamonds: 500,
         activeExpedition: null,
         expeditionStartTime: null,
         expeditionDuration: 0,
